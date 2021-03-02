6f0e3a2c7a89bf8ab21627110c7036b1f0fe91a5
ceyda-albayrak <ceydaa2098@gmail.com>
2021-03-03 01:48:30 +0300

    refactor: table transactions

--- djangoSite/Blogs/models.py
+++ djangoSite/Blogs/models.py
@@ -12,4 +12,3 @@ class Blog(models.Model):
     def __str__(self):
         return self.title
         
- 
--- djangoSite/Blogs/views.py
+++ djangoSite/Blogs/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import Blog
 from CurriculumVitae.models import CV
 from django.views.generic import TemplateView
 
 # Create your views here.
 
 class BlogListView(ListView):
-    model=Blog
+    
     template_name='cv-information.html'
 
 class AnasayfaView(TemplateView):
